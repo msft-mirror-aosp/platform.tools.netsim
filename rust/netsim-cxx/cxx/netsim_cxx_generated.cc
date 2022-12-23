6fa7ebf9a4a05748fcd331c992d16aec4f8936e0
Bill Schilit <schilit@google.com>
2022-12-22 16:54:16 -0800

    Move ranging cxx into the netsim-cxx crate to avoid duplicate symbols on macos builds.
    Remove packet_streamer cxx it is not yet ready to build on aosp.
    
    test: ninja -C objs netsimd
    Change-Id: Ie6f3c7abe62be62c48472a5e18f93b8e5c283fef

--- rust/netsim-cxx/cxx/netsim_cxx_generated.cc
+++ rust/netsim-cxx/cxx/netsim_cxx_generated.cc
@@ -1,86 +1,17 @@
-#include "backend/backend_server.h"
-#include <cstddef>
-#include <memory>
-#include <new>
-#include <string>
-#include <type_traits>
-#include <utility>
-
-namespace rust {
-inline namespace cxxbridge1 {
-// #include "rust/cxx.h"
-
-#ifndef CXXBRIDGE1_IS_COMPLETE
-#define CXXBRIDGE1_IS_COMPLETE
-namespace detail {
-namespace {
-template <typename T, typename = std::size_t>
-struct is_complete : std::false_type {};
-template <typename T>
-struct is_complete<T, decltype(sizeof(T))> : std::true_type {};
-} // namespace
-} // namespace detail
-#endif // CXXBRIDGE1_IS_COMPLETE
-
-namespace {
-template <bool> struct deleter_if {
-  template <typename T> void operator()(T *) {}
-};
-
-template <> struct deleter_if<true> {
-  template <typename T> void operator()(T *ptr) { ptr->~T(); }
-};
-} // namespace
-} // namespace cxxbridge1
-} // namespace rust
-
-namespace netsim {
-  using PacketStreamClient = ::netsim::PacketStreamClient;
-}
+#include <cstdint>
 
 namespace netsim {
 extern "C" {
-void netsim$cxxbridge1$stream_packets_handler(::netsim::PacketStreamClient *packet_stream_client) noexcept;
-
 void netsim$cxxbridge1$run_frontend_http_server() noexcept;
 
-::std::string *netsim$cxxbridge1$PacketStreamClient$read(::netsim::PacketStreamClient const &self) noexcept {
-  ::std::unique_ptr<::std::string> (::netsim::PacketStreamClient::*read$)() const = &::netsim::PacketStreamClient::Read;
-  return (self.*read$)().release();
-}
-
-void netsim$cxxbridge1$PacketStreamClient$write(::netsim::PacketStreamClient const &self, ::std::string const &response) noexcept {
-  void (::netsim::PacketStreamClient::*write$)(::std::string const &) const = &::netsim::PacketStreamClient::Write;
-  (self.*write$)(response);
-}
+::std::int8_t netsim$cxxbridge1$distance_to_rssi(::std::int8_t tx_power, float distance) noexcept;
 } // extern "C"
-
-void StreamPacketHandler(::std::unique_ptr<::netsim::PacketStreamClient> packet_stream_client) noexcept {
-  netsim$cxxbridge1$stream_packets_handler(packet_stream_client.release());
-}
 
 void RunFrontendHttpServer() noexcept {
   netsim$cxxbridge1$run_frontend_http_server();
 }
-} // namespace netsim
 
-extern "C" {
-static_assert(::rust::detail::is_complete<::netsim::PacketStreamClient>::value, "definition of PacketStreamClient is required");
-static_assert(sizeof(::std::unique_ptr<::netsim::PacketStreamClient>) == sizeof(void *), "");
-static_assert(alignof(::std::unique_ptr<::netsim::PacketStreamClient>) == alignof(void *), "");
-void cxxbridge1$unique_ptr$netsim$PacketStreamClient$null(::std::unique_ptr<::netsim::PacketStreamClient> *ptr) noexcept {
-  ::new (ptr) ::std::unique_ptr<::netsim::PacketStreamClient>();
+::std::int8_t DistanceToRssi(::std::int8_t tx_power, float distance) noexcept {
+  return netsim$cxxbridge1$distance_to_rssi(tx_power, distance);
 }
-void cxxbridge1$unique_ptr$netsim$PacketStreamClient$raw(::std::unique_ptr<::netsim::PacketStreamClient> *ptr, ::netsim::PacketStreamClient *raw) noexcept {
-  ::new (ptr) ::std::unique_ptr<::netsim::PacketStreamClient>(raw);
-}
-::netsim::PacketStreamClient const *cxxbridge1$unique_ptr$netsim$PacketStreamClient$get(::std::unique_ptr<::netsim::PacketStreamClient> const &ptr) noexcept {
-  return ptr.get();
-}
-::netsim::PacketStreamClient *cxxbridge1$unique_ptr$netsim$PacketStreamClient$release(::std::unique_ptr<::netsim::PacketStreamClient> &ptr) noexcept {
-  return ptr.release();
-}
-void cxxbridge1$unique_ptr$netsim$PacketStreamClient$drop(::std::unique_ptr<::netsim::PacketStreamClient> *ptr) noexcept {
-  ::rust::deleter_if<::rust::detail::is_complete<::netsim::PacketStreamClient>::value>{}(ptr);
-}
-} // extern "C"
+} // namespace netsim
