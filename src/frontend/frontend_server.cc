012209fb21ddfeb8a29309ba569c7ddcd94be6da
Bailey Kuo <formosa@google.com>
2023-03-08 12:32:09 -0800

    Build cxx by build.rs
    - Build cxx files automatically
    
    Test: ninja
    Bug: 271901850
    Change-Id: I1dfac53b08f5f365addf0fa444a3935e15831f93

--- src/frontend/frontend_server.cc
+++ src/frontend/frontend_server.cc
@@ -25,7 +25,7 @@
 #include "google/protobuf/empty.pb.h"
 #include "grpcpp/server_context.h"
 #include "grpcpp/support/status.h"
-#include "netsim_cxx_generated.h"
+#include "netsim-cxx/src/lib.rs.h"
 
 namespace netsim {
 namespace {
