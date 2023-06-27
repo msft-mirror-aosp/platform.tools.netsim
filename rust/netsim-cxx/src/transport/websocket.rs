ca9e4e700377811a451842965d12070ac235fce2
Kuen Yuet Cheung <kuenyuet@google.com>
2023-06-27 04:44:06 +0000

    Revert "Perform handshake protocol to provide Sec-Websocket-Acce..."
    Revert submission 2622504
    
    Reason for revert: DroidMonitor: Potential culprit for Bug 288975937 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Reverted changes: /q/submissionid:2622504
    
    Change-Id: I09e8a495ebc608e804a49dfc69f30631a4f6b3b9

--- rust/netsim-cxx/src/transport/websocket.rs
+++ rust/netsim-cxx/src/transport/websocket.rs
@@ -16,44 +16,6 @@ use std::{collections::HashMap, net::TcpStream};
 
 use log::info;
 
-use crate::http_server::{
-    collect_query, http_request::HttpRequest, server_response::ResponseWritable,
-};
-
-// This feature is enabled only for CMake builds
-#[cfg(feature = "local_ssl")]
-use crate::openssl;
-
-/// Generate Sec-Websocket-Accept value from given Sec-Websocket-Key value
-fn generate_websocket_accept(websocket_key: String) -> String {
-    let concat = websocket_key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-    let hashed = openssl::sha::sha1(concat.as_bytes());
-    data_encoding::BASE64.encode(&hashed)
-}
-
-/// Check if all required fields exist in queries and request headers
-fn has_required_websocket_fields(queries: &HashMap<&str, &str>, request: &HttpRequest) -> bool {
-    queries.contains_key("name")
-        && queries.contains_key("kind")
-        && request.headers.get("Sec-Websocket-Key").is_some()
-}
-
-/// Handler for websocket server connection
-pub fn handle_websocket(request: &HttpRequest, param: &str, writer: ResponseWritable) {
-    match collect_query(param) {
-        Ok(queries) if has_required_websocket_fields(&queries, request) => {
-            let websocket_accept =
-                generate_websocket_accept(request.headers.get("Sec-Websocket-Key").unwrap());
-            writer.put_ok_switch_protocol(
-                "websocket",
-                &[("Sec-WebSocket-Accept", websocket_accept.as_str())],
-            )
-        }
-        Ok(_) => writer.put_error(404, "Missing query fields and/or Sec-Websocket-Key"),
-        Err(err) => writer.put_error(404, err),
-    }
-}
-
 pub fn run_websocket_transport(_stream: TcpStream, queries: HashMap<&str, &str>) {
     info!("Running Websocket server");
     info!("Queries for Websocket: {queries:?}");
