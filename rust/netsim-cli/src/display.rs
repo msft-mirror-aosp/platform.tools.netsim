b26e6892c2623aeb19032a990388190bb029923e
James Eidson <jmes@google.com>
2023-07-28 18:44:26 +0000

    Refactor `netsim devices` response handler
    * Moved `nesim devices` response handler function into display module
    * Split implementation into components for each displayable proto
    
    Bug: 293329329
    Test: netsimd && nimble_scanner && netsim -v devices
    Test: netsim && launch_cvd -netsim --num_instances=2 & netsim -v devices
    Change-Id: I6193ba6ec980aeb71e09befe8ce7bee4eb78b4da

--- rust/netsim-cli/src/display.rs
+++ rust/netsim-cli/src/display.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use frontend_proto::frontend::ListDeviceResponse;
 use frontend_proto::model::{
     self,
     chip::bluetooth_beacon::{AdvertiseData, AdvertiseSettings},
@@ -19,7 +20,7 @@ use frontend_proto::model::{
 use protobuf::MessageField;
 use std::fmt;
 
-pub const INDENT_INCREMENT: usize = 2;
+const INDENT_WIDTH: usize = 2;
 
 /// Displayer for model protobufs. Implements fmt::Display.
 /// # Invariants
@@ -38,8 +39,146 @@ impl<T> Displayer<T> {
 
     /// Indent the displayed string by a given amount. Returns `self`.
     pub fn indent(&mut self, indent: usize) -> &Self {
-        self.indent = indent;
+        self.indent = indent * INDENT_WIDTH;
         self
+    }
+}
+
+impl fmt::Display for Displayer<ListDeviceResponse> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let indent = self.indent;
+
+        let mut devices = self.value.devices.iter().peekable();
+        while let Some(device) = devices.next() {
+            write!(f, "{:indent$}{}", "", Displayer::new(device, self.verbose))?;
+            if devices.peek().is_some() {
+                writeln!(f)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl fmt::Display for Displayer<&model::Device> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let indent = self.indent;
+        let width = 9;
+
+        write!(
+            f,
+            "{:indent$}{:width$} {}",
+            "",
+            self.value.name,
+            Displayer::new(self.value.position.as_ref().unwrap_or_default(), self.verbose)
+        )?;
+
+        let mut chips = self.value.chips.iter().peekable();
+        while let Some(chip) = chips.next() {
+            write!(
+                f,
+                "{:indent$}{}",
+                "",
+                Displayer::new(chip, self.verbose).indent(self.indent + 1)
+            )?;
+            if chips.peek().is_some() {
+                writeln!(f)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl fmt::Display for Displayer<&model::Chip> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let indent = self.indent;
+        let width = 9;
+
+        match self.value.chip.as_ref() {
+            Some(model::chip::Chip::BleBeacon(ble_beacon)) => {
+                let radio = ble_beacon.bt.low_energy.as_ref().unwrap_or_default();
+                let beacon_width = 16 + width;
+
+                if self.verbose || radio.state.enum_value_or_default() == model::State::OFF {
+                    writeln!(f)?;
+                    writeln!(
+                        f,
+                        "{:indent$}{:beacon_width$} {}",
+                        "",
+                        format!("beacon-ble ({}):", self.value.name),
+                        Displayer::new(radio, self.verbose),
+                    )?;
+                }
+
+                if self.verbose {
+                    write!(
+                        f,
+                        "{}",
+                        Displayer::new(ble_beacon, self.verbose).indent(self.indent + 1)
+                    )?;
+                }
+            }
+            Some(model::chip::Chip::Bt(bt)) => {
+                if let Some(ble) = bt.low_energy.as_ref() {
+                    if self.verbose || ble.state.enum_value_or_default() == model::State::OFF {
+                        writeln!(f)?;
+                        write!(
+                            f,
+                            "{:indent$}{:width$}{}",
+                            "",
+                            "ble: ",
+                            Displayer::new(ble, self.verbose),
+                        )?;
+                    }
+                };
+
+                if let Some(classic) = bt.classic.as_ref() {
+                    if self.verbose || classic.state.enum_value_or_default() == model::State::OFF {
+                        writeln!(f)?;
+                        write!(
+                            f,
+                            "{:indent$}{:width$}{}",
+                            "",
+                            "classic: ",
+                            Displayer::new(classic, self.verbose),
+                        )?;
+                    }
+                };
+            }
+            Some(model::chip::Chip::Wifi(wifi)) => {
+                if self.verbose || wifi.state.enum_value_or_default() == model::State::OFF {
+                    writeln!(f)?;
+                    write!(
+                        f,
+                        "{:indent$}{:width$}{}",
+                        "",
+                        "wifi: ",
+                        Displayer::new(wifi, self.verbose)
+                    )?;
+                }
+            }
+            Some(model::chip::Chip::Uwb(uwb)) => {
+                if self.verbose || uwb.state.enum_value_or_default() == model::State::OFF {
+                    writeln!(f)?;
+                    write!(
+                        f,
+                        "{:indent$}{:width$}{}",
+                        "",
+                        "uwb: ",
+                        Displayer::new(uwb, self.verbose)
+                    )?;
+                }
+            }
+            _ => {
+                if self.verbose {
+                    writeln!(f)?;
+                    write!(f, "{:indent$}Unknown chip", "")?
+                }
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -58,7 +197,7 @@ impl fmt::Display for Displayer<&model::chip::BluetoothBeacon> {
                 "{:indent$}advertise settings:\n{}",
                 "",
                 Displayer::new(self.value.settings.as_ref().unwrap_or_default(), self.verbose)
-                    .indent(self.indent + INDENT_INCREMENT)
+                    .indent(self.indent + 1)
             )?;
         }
 
@@ -71,7 +210,7 @@ impl fmt::Display for Displayer<&model::chip::BluetoothBeacon> {
                 "{:indent$}advertise packet data:\n{}",
                 "",
                 Displayer::new(self.value.adv_data.as_ref().unwrap_or_default(), self.verbose)
-                    .indent(self.indent + INDENT_INCREMENT)
+                    .indent(self.indent + 1)
             )?;
         }
 
