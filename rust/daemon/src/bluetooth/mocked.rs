bfff8bbfeb6eadde9fe954ddf6bca09d4ba11e28
Hyun Jae Moon <hyunjaemoon@google.com>
2023-11-28 19:16:57 +0000

    Have ble_beacon use echip::get and remove all facade.rs
    Test: m netsimd
    Bug: 313672213
    Change-Id: Ic7b7c333c5c5067f35ce22e6e4cdf909ea07168f

--- rust/daemon/src/bluetooth/mocked.rs
+++ rust/daemon/src/bluetooth/mocked.rs
@@ -41,54 +41,6 @@ impl FacadeIds {
     }
 }
 
-pub fn handle_bluetooth_request(facade_id: u32, packet_type: u8, packet: &Vec<u8>) {
-    info!("hci_reset({facade_id}, {packet_type}, {packet:?})");
-}
-
-pub fn bluetooth_reset(facade_id: u32) {
-    info!("hci_reset({facade_id})");
-}
-
-pub fn bluetooth_remove(facade_id: u32) {
-    info!("hci_remove({facade_id})");
-}
-
-pub fn bluetooth_patch(facade_id: u32, bluetooth: &Bluetooth) {
-    info!("hci_patch({facade_id}, {bluetooth:?})");
-}
-
-pub fn bluetooth_get(facade_id: u32) -> Bluetooth {
-    info!("hci_get({facade_id})");
-    Bluetooth::new()
-}
-
-// Returns facade_id
-pub fn bluetooth_add(
-    device_id: u32,
-    _address: &str,
-    _bt_properties: &MessageField<RootcanalController>,
-) -> u32 {
-    info!("hci_add({device_id})");
-    let mut resource = IDS.write().unwrap();
-    let facade_id = resource.current_id;
-    resource.current_id += 1;
-    facade_id
-}
-
-/// Starts the Bluetooth service.
-pub fn bluetooth_start(
-    _config: &MessageField<BluetoothConfig>,
-    _instance_num: u16,
-    _disable_address_reuse: bool,
-) {
-    info!("bluetooth service started");
-}
-
-/// Stops the Bluetooth service.
-pub fn bluetooth_stop() {
-    info!("bluetooth service ended");
-}
-
 // Avoid crossing cxx boundary in tests
 pub fn ble_beacon_add(
     device_id: DeviceIdentifier,
