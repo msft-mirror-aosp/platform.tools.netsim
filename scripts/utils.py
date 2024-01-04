10902305998f1ddd6456eafe802b029b427e5df0
Erwin Jansen <jansene@google.com>
2024-01-04 10:34:07 -0800

    Fix netsim build dependencies
    This makes sure we can build netsim from the right directories.
    
    Bug: 288461870
    Change-Id: Id36363c66bd6a95d2579396b0d8f5beff0ce92f9

--- scripts/utils.py
+++ scripts/utils.py
@@ -40,9 +40,11 @@ TARGET_MAP = {
     "windows_x64": "windows_msvc-x86_64",
     "linux": "linux-x86_64",
     "linux_x64": "linux-x86_64",
+    "linux_x86_64": "linux-x86_64",
+    "linux_aarch64": "linux-aarch64",
     "darwin": "darwin-x86_64",
     "darwin_x64": "darwin-x86_64",
-    "linux_aarch64": "linux-aarch64",
+    "darwin_x86_64" : "darwin-x86_64",
     "darwin_aarch64": "darwin-aarch64",
 }
 
@@ -57,7 +59,7 @@ AVAILABLE = {
 
 def platform_to_cmake_target(target):
     """Translates platform to cmake target"""
-    return TARGET_MAP[target]
+    return TARGET_MAP[target.replace("-","_")]
 
 
 def cmake_toolchain(target) -> str:
@@ -69,7 +71,7 @@ def cmake_toolchain(target) -> str:
         / "android"
         / "build"
         / "cmake"
-        / AVAILABLE[TARGET_MAP[target]]
+        / AVAILABLE[TARGET_MAP[target.replace("-", "_")]]
     )
 
 
