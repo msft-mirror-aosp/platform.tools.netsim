972f241bf5f9e10b6dd1b9f5652cfdfe3b2158f0
Hyun Jae Moon <hyunjaemoon@google.com>
2024-02-23 20:40:19 +0000

    Replace HttpResponse struct with http::Response
    Deleted http_response.rs and the necessary methods are defined in
    server_response.rs
    
    Test: scripts/cmake_setup.py && ninja -C objs
    Bug: 293512425
    Change-Id: Idc2d007bdd180ae081fafa1ab19bc8fe58f13083

--- rust/daemon/src/http_server/server_response.rs
+++ rust/daemon/src/http_server/server_response.rs
@@ -23,10 +23,10 @@
 //! This library is intended solely for serving netsim clients.
 
 use std::io::Write;
+use std::str::FromStr;
 
+use http::{HeaderName, HeaderValue, Response};
 use log::error;
-
-use crate::http_server::http_response::HttpResponse;
 
 pub type ResponseWritable<'a> = &'a mut dyn ServerResponseWritable;
 
@@ -46,33 +46,34 @@ pub trait ServerResponseWritable {
 // A response writer that can contain a TCP stream or other writable.
 pub struct ServerResponseWriter<'a> {
     writer: &'a mut dyn Write,
-    response: Option<HttpResponse>,
+    response: Option<Response<Vec<u8>>>,
 }
 
 impl<'a> ServerResponseWriter<'a> {
     pub fn new<W: Write>(writer: &mut W) -> ServerResponseWriter {
         ServerResponseWriter { writer, response: None }
     }
-    pub fn put_response(&mut self, response: HttpResponse) {
-        let reason = match response.status_code {
+    pub fn put_response(&mut self, response: Response<Vec<u8>>) {
+        let reason = match response.status().as_u16() {
             101 => "Switching Protocols",
             200 => "OK",
             404 => "Not Found",
             _ => "Unknown Reason",
         };
-        let mut buffer = format!("HTTP/1.1 {} {}\r\n", response.status_code, reason).into_bytes();
-        for (name, value) in response.headers.iter() {
+        let mut buffer =
+            format!("HTTP/1.1 {} {}\r\n", response.status().as_str(), reason).into_bytes();
+        for (name, value) in response.headers() {
             buffer
                 .extend_from_slice(format!("{}: {}\r\n", name, value.to_str().unwrap()).as_bytes());
         }
         buffer.extend_from_slice(b"\r\n");
-        buffer.extend_from_slice(&response.body);
+        buffer.extend_from_slice(response.body());
         if let Err(e) = self.writer.write_all(&buffer) {
             error!("handle_connection error {e}");
         };
         self.response = Some(response);
     }
-    pub fn get_response(self) -> Option<HttpResponse> {
+    pub fn get_response(self) -> Option<Response<Vec<u8>>> {
         self.response
     }
 }
@@ -82,8 +83,18 @@ impl<'a> ServerResponseWriter<'a> {
 // by the handler methods.
 impl ServerResponseWritable for ServerResponseWriter<'_> {
     fn put_error(&mut self, error_code: u16, error_message: &str) {
-        let response = HttpResponse::new_error(error_code, error_message.into());
-        self.put_response(response);
+        let body = error_message.as_bytes().to_vec();
+        self.put_response(
+            Response::builder()
+                .status(error_code)
+                .header("Content-Type", HeaderValue::from_static("text/plain"))
+                .header(
+                    "Content-Length",
+                    HeaderValue::from_str(body.len().to_string().as_str()).unwrap(),
+                )
+                .body(body)
+                .unwrap(),
+        );
     }
     fn put_chunk(&mut self, chunk: &[u8]) {
         if let Err(e) = self.writer.write_all(chunk) {
@@ -92,24 +103,52 @@ impl ServerResponseWritable for ServerResponseWriter<'_> {
         self.writer.flush().unwrap();
     }
     fn put_ok_with_length(&mut self, mime_type: &str, length: usize, headers: StrHeaders) {
-        let mut response = HttpResponse::new_ok_with_length(mime_type, length);
-        response.add_headers(headers);
+        let mut response = Response::builder()
+            .status(200)
+            .header("Content-Type", HeaderValue::from_str(mime_type).unwrap())
+            .header("Content-Length", HeaderValue::from_str(length.to_string().as_str()).unwrap())
+            .body(Vec::new())
+            .unwrap();
+        add_headers(&mut response, headers);
         self.put_response(response);
     }
     fn put_ok(&mut self, mime_type: &str, body: &str, headers: StrHeaders) {
-        let mut response = HttpResponse::new_ok(mime_type, body.into());
-        response.add_headers(headers);
+        let mut response = new_ok(mime_type, body.into());
+        add_headers(&mut response, headers);
         self.put_response(response);
     }
     fn put_ok_with_vec(&mut self, mime_type: &str, body: Vec<u8>, headers: StrHeaders) {
-        let mut response = HttpResponse::new_ok(mime_type, body);
-        response.add_headers(headers);
+        let mut response = new_ok(mime_type, body);
+        add_headers(&mut response, headers);
         self.put_response(response);
     }
     fn put_ok_switch_protocol(&mut self, connection: &str, headers: StrHeaders) {
-        let mut response = HttpResponse::new_ok_switch_protocol(connection);
-        response.add_headers(headers);
+        let mut response = Response::builder()
+            .status(101)
+            .header("Upgrade", HeaderValue::from_str(connection).unwrap())
+            .header("Connection", HeaderValue::from_static("Upgrade"))
+            .body(Vec::new())
+            .unwrap();
+        add_headers(&mut response, headers);
         self.put_response(response);
+    }
+}
+
+fn new_ok(content_type: &str, body: Vec<u8>) -> Response<Vec<u8>> {
+    Response::builder()
+        .status(200)
+        .header("Content-Type", HeaderValue::from_str(content_type).unwrap())
+        .header("Content-Length", HeaderValue::from_str(body.len().to_string().as_str()).unwrap())
+        .body(body)
+        .unwrap()
+}
+
+fn add_headers(response: &mut Response<Vec<u8>>, headers: StrHeaders) {
+    for (key, value) in headers {
+        response.headers_mut().insert(
+            HeaderName::from_str(key.as_str()).unwrap(),
+            HeaderValue::from_str(value.as_str()).unwrap(),
+        );
     }
 }
 
@@ -117,6 +156,8 @@ impl ServerResponseWritable for ServerResponseWriter<'_> {
 mod tests {
     use super::*;
     use std::io::Cursor;
+
+    const SAMPLE_CHUNK: &[u8] = &[0, 1, 2, 3, 4];
 
     #[test]
     fn test_put_error() {
@@ -137,6 +178,51 @@ mod tests {
         let written_bytes = stream.get_ref();
         let expected_bytes =
             b"HTTP/1.1 200 OK\r\ncontent-type: text/plain\r\ncontent-length: 11\r\n\r\nHello World";
+        assert_eq!(written_bytes, expected_bytes);
+    }
+
+    #[test]
+    fn test_put_ok_with_length() {
+        let mut stream = Cursor::new(Vec::new());
+        let mut writer = ServerResponseWriter::new(&mut stream);
+        writer.put_ok_with_length("text/plain", 100, vec![]);
+        let written_bytes = stream.get_ref();
+        let expected_bytes =
+            b"HTTP/1.1 200 OK\r\ncontent-type: text/plain\r\ncontent-length: 100\r\n\r\n";
+        assert_eq!(written_bytes, expected_bytes);
+    }
+
+    #[test]
+    fn test_put_ok_with_vec() {
+        let mut stream = Cursor::new(Vec::new());
+        let mut writer = ServerResponseWriter::new(&mut stream);
+        writer.put_ok_with_vec("text/plain", SAMPLE_CHUNK.to_vec(), vec![]);
+        let written_bytes = stream.get_ref();
+        let expected_bytes = &[
+            b"HTTP/1.1 200 OK\r\ncontent-type: text/plain\r\ncontent-length: 5\r\n\r\n".to_vec(),
+            SAMPLE_CHUNK.to_vec(),
+        ]
+        .concat();
+        assert_eq!(written_bytes, expected_bytes);
+    }
+
+    #[test]
+    fn test_put_ok_switch_protocol() {
+        let mut stream = Cursor::new(Vec::new());
+        let mut writer = ServerResponseWriter::new(&mut stream);
+        writer.put_ok_switch_protocol("Websocket", vec![]);
+        let written_bytes = stream.get_ref();
+        let expected_bytes = b"HTTP/1.1 101 Switching Protocols\r\nupgrade: Websocket\r\nconnection: Upgrade\r\n\r\n";
+        assert_eq!(written_bytes, expected_bytes);
+    }
+
+    #[test]
+    fn test_put_chunk() {
+        let mut stream = Cursor::new(Vec::new());
+        let mut writer = ServerResponseWriter::new(&mut stream);
+        writer.put_chunk(SAMPLE_CHUNK);
+        let written_bytes = stream.get_ref();
+        let expected_bytes = SAMPLE_CHUNK;
         assert_eq!(written_bytes, expected_bytes);
     }
 }
