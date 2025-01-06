81eb7b1f31f36fc9516eaabb7385845124cab8a9
Chris Wailes <chriswailes@google.com>
2025-01-06 14:31:30 -0800

    Update code for Rust 1.83.0
    Test: m rust
    Bug: 384401387
    Change-Id: I8fde6deea2a836a455a07b1d5a4aa7a6b9cd8f63

--- rust/daemon/src/version.rs
+++ rust/daemon/src/version.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::empty_line_after_doc_comments)]
+
 /// Version library.
 
 pub const VERSION: &str = "0.3.41";
